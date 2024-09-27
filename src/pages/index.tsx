cc184fa1f28dabda5c9c530601c1c7150fd932b3
shakthi.m <shakthi.m@go-mmt.com>
2024-09-27 18:52:33 +0530

    idkwftiadd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -120,16 +120,17 @@ export default function Home() {
                     </Select>
                 </div>
             </div>
-            <div className="flex items-center space-x-2 p-4 bg-gray-100 rounded">
+            <div className="flex bg-gray-100 rounded overflow-hidden">
                 <div className="flex-grow">
-                    <p className="text-sm font-mono break-all">{url}</p>
+                    <p className="text-sm font-mono break-all p-4 space-x-2">{url}</p>
                 </div>
-                <Button onClick={copyToClipboard} variant="ghost" size="icon">
-                    <ClipboardCopy className="h-4 w-4" color={'grey'} />
+                <Button onClick={copyToClipboard} className="h-auto p-4 bg-gray-950 border-l border-gray-300 rounded-none">
+                    <ClipboardCopy className="h-4 w-4" color={'white'}/>
                 </Button>
             </div>
+
             <div className="border border-gray-300 rounded p-4">
-                <img src={url} alt="Generated Placeholder" className="mx-auto" />
+                <img src={url} alt="Generated Placeholder" className="mx-auto"/>
             </div>
         </div>
     )
