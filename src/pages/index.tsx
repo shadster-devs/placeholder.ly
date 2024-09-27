ed146ef7b5f1174ae53ef2e7c9431ad2bc600f1f
shakthi.m <shakthi.m@go-mmt.com>
2024-09-27 19:24:29 +0530

    idkwftiad #4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,6 +34,8 @@ export default function Home() {
         navigator.clipboard.writeText(url)
     }
 
+    console.log(   process.cwd())
+
     return (
         <div className="max-w-2xl mx-auto p-6 space-y-6">
             <h1 className="text-2xl font-bold text-center">Placeholder.ly</h1>
